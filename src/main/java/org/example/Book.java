214ba4b770ff6e68f26a373d8ec8e03afa261bb8
Sergiofer1994 <sergiofer1994@gmail.com>
2026-02-16 18:08:52 +0100

    feat: title getter and setter created

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Book {
+    private  String titñe, destription, isbn, writer;
+    private double price;
+
+    public String getTitñe() {
+        return titñe;
+    }
+
+    public void setTitñe(String titñe) {
+        this.titñe = titñe;
+    }
+}
